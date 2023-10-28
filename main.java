a2997b0cde78aae17a22b597ad393ba80f668f1d
Chonarue <ismael.chonarueda@mail.utoronto.ca>
2023-10-28 14:22:36 -0400

    Added creditCard class, including a constructor and getter and setters for all instance attributes. Edit: also added Review and reformated a little

--- main.java
+++
@@ -1,5 +0,0 @@
-public class MyApp {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
--- tests/ReviewTests.java
+++ tests/ReviewTests.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import Review;
+import org.example.src.entities.Review;
 
 public class ReviewTests{
     @Test
