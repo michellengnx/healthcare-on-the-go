794dd046dcd36cc8b22e77380b94e60133fc9b22
Chonarue <ismael.chonarueda@mail.utoronto.ca>
2023-10-30 16:25:38 -0400

    Added creditCard class, including a constructor and getter and setters for all instance attributes. Edit: also added Review and reformated a little: October 30th: added test case to review and added test directory also imported dependencies for other classes.

---
+++ src/entities/Review.java
@@ -0,0 +1,52 @@
+package entities;
+import java.util.Date;
+
+public class Review {
+        private Doctor doctor;
+        private String review;
+        private Date time;
+        private int stars; // how many stars for the review can be from 0 to 5
+
+        //preconditions: 0 <= stars <= 5
+
+        public Review(String review, Date time, int stars, Doctor doctor){
+            this.review = review;
+            this.time = time;
+            this.stars = stars;
+            this.doctor = doctor;
+
+        }
+
+        public String getReview() {
+            return this.review;
+        }
+
+        public Date getTime() {
+            return this.time;
+        }
+
+        public int getStars() {
+            return this.stars;
+        }
+
+        public Doctor getDoctor(){
+            return this.doctor;
+        }
+
+        public void setReview(String review) {
+            this.review = review;
+        }
+
+        public void setStars(int stars) {
+            this.stars = stars;
+        }
+
+        public void setTime(Date time) {
+            this.time = time;
+        }
+
+        public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+        }
+}
+
