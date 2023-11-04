0c9220e33c4f6bcb3832a73c04da221b19cdb9d7
armaan_n <armaannanji@gmail.com>
2023-11-03 23:01:40 -0400

    added constructor and getters to input data object

--- src/use_case/CreateRequest/CreateRequestInputData.java
+++ src/use_case/CreateRequest/CreateRequestInputData.java
@@ -1,4 +1,35 @@
 package use_case.CreateRequest;
 
+import entities.Service;
+
+import java.util.Date;
+
 public class CreateRequestInputData {
+    Date creationTime;
+    int urgencyLevel;
+    String destination;
+    Service service;
+
+    public CreateRequestInputData(Date creationTime, int urgencyLevel, String destination, Service service) {
+        this.creationTime = creationTime;
+        this.urgencyLevel = urgencyLevel;
+        this.destination = destination;
+        this.service = service;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public int getUrgencyLevel() {
+        return urgencyLevel;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public Service getService() {
+        return service;
+    }
 }
