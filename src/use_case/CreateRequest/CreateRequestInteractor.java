ff7a428e80838d7016c03d90a47df4419818eddf
armaan_n <armaannanji@gmail.com>
2023-11-04 09:58:41 -0400

    implemented core logic for the creat request interactor

--- src/use_case/CreateRequest/CreateRequestInteractor.java
+++ src/use_case/CreateRequest/CreateRequestInteractor.java
@@ -1,8 +1,15 @@
 package use_case.CreateRequest;
 
 import entities.Doctor;
+import entities.Patient;
+import entities.ServiceRequest;
+import entities.factories.service_request.ServiceRequestFactory;
+import entities.matchers.DoctorMatcher;
+import entities.Service;
+import entities.matchers.DoctorMatcherApiAccessInterface;
+import entities.matchers.DoctorMatcherDataAccessInterface;
 
-import java.util.List;
+import java.util.Date;
 
 public class CreateRequestInteractor {
     final CreateRequestApiAccessInterface apiAccessObject;
@@ -21,6 +28,42 @@ public class CreateRequestInteractor {
     }
 
     public void execute(CreateRequestInputData createRequestInputData) {
+        Service requestedService = createRequestInputData.getService();
+        String destination = createRequestInputData.getDestination();
+        Date creationTime = createRequestInputData.getCreationTime();
+        int urgencyLevel = createRequestInputData.getUrgencyLevel();
+        Patient patient = createRequestInputData.getPatient();
 
+        DoctorMatcher matcher = new DoctorMatcher(
+                requestedService,
+                destination,
+                (DoctorMatcherDataAccessInterface) this.doctorDataAccessObject,
+                (DoctorMatcherApiAccessInterface) this.apiAccessObject);
+        Doctor matchedDoctor;
+
+        try {
+            matchedDoctor = matcher.match();
+            float price = this.apiAccessObject.getPrice(matchedDoctor.getLocation(), destination);
+            float eta = this.apiAccessObject.getEta(matchedDoctor.getLocation(), destination);
+            float distance = this.apiAccessObject.getDistance(matchedDoctor.getLocation(), destination);
+
+            ServiceRequestFactory serviceRequestFactory = new ServiceRequestFactory();
+
+            ServiceRequest request = serviceRequestFactory.create(
+                    creationTime,
+                    matchedDoctor,
+                    urgencyLevel,
+                    destination,
+                    requestedService,
+                    price,
+                    eta,
+                    distance
+            );
+
+            this.userDataAccessObject.saveRequest(patient, request);
+            this.completeRequestPresenter.prepareSuccessView(request);
+        } catch (NoAvailableDoctorException e) {
+            this.completeRequestPresenter.prepareFailView("No Doctors Available!");
+        }
     }
 }
