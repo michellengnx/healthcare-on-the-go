d166da7c24f9374face4722829a1d2b4ce2e9940
armaan_n <armaannanji@gmail.com>
2023-11-04 09:26:29 -0400

    removed need for api access in entity layer

--- src/entities/factories/service_request/ServiceRequestFactoryApiAccessInterface.java
+++
@@ -1,7 +0,0 @@
-package entities.factories.service_request;
-
-public interface ServiceRequestFactoryApiAccessInterface {
-    public float getDistance(String startLoc, String endLoc);
-    public float getEta(String startLoc, String endLoc);
-    public float getPrice(String startLoc, String endLoc);
-}
