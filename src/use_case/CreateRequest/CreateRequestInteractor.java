678784409d00c35f8ab960e4b1548e1106b489ba
armaan_n <armaannanji@gmail.com>
2023-11-03 23:12:01 -0400

    added constructor and empty execute method to interactor

--- src/use_case/CreateRequest/CreateRequestInteractor.java
+++ src/use_case/CreateRequest/CreateRequestInteractor.java
@@ -1,4 +1,26 @@
 package use_case.CreateRequest;
 
+import entities.Doctor;
+
+import java.util.List;
+
 public class CreateRequestInteractor {
+    final CreateRequestApiAccessInterface apiAccessObject;
+    final CreateRequestDoctorDataAccessInterface doctorDataAccessObject;
+    final CreateRequestUserDataAccessInterface userDataAccessObject;
+    final CreateRequestOutputBoundary completeRequestPresenter;
+
+    public CreateRequestInteractor(CreateRequestApiAccessInterface apiAccessObject,
+                                   CreateRequestDoctorDataAccessInterface doctorDataAccessObject,
+                                   CreateRequestUserDataAccessInterface userDataAccessObject,
+                                   CreateRequestOutputBoundary completeRequestPresenter) {
+        this.apiAccessObject = apiAccessObject;
+        this.doctorDataAccessObject = doctorDataAccessObject;
+        this.userDataAccessObject = userDataAccessObject;
+        this.completeRequestPresenter = completeRequestPresenter;
+    }
+
+    public void execute(CreateRequestInputData createRequestInputData) {
+
+    }
 }
