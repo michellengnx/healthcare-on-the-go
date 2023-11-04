ed44e8afa5b4fc1708c418a95c0ea8f46514b713
armaan_n <armaannanji@gmail.com>
2023-11-03 23:57:30 -0400

    added additional method signature for getting doctors with a particular service

--- src/use_case/CreateRequest/CreateRequestDoctorDataAccessInterface.java
+++ src/use_case/CreateRequest/CreateRequestDoctorDataAccessInterface.java
@@ -1,9 +1,11 @@
 package use_case.CreateRequest;
 
 import entities.Doctor;
+import entities.Service;
 
 import java.util.List;
 
 public interface CreateRequestDoctorDataAccessInterface {
     public List<Doctor> getAvailableDoctors();
+    public List<Doctor> getAvailableDoctors(Service service);
 }
