e2e4585711c8dc87d91ad3eaf99eb8ed29025225
armaan_n <armaannanji@gmail.com>
2023-11-04 10:13:46 -0400

    added boilerplate code for interface adapters

---
+++ src/interface_adapter/CreateRequest/CreateRequestController.java
@@ -0,0 +1,9 @@
+package interface_adapter.CreateRequest;
+
+import entities.Patient;
+import use_case.CreateRequest.CreateRequestInputBoundary;
+import use_case.CreateRequest.CreateRequestInputData;
+
+public class CreateRequestController {
+
+}
