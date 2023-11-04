13c58893da063a62eb31d645c3fd5c22bde8c060
armaan_n <armaannanji@gmail.com>
2023-11-04 11:58:45 -0400

    implemented create presenter, note that this won't run until the view request use case is implemented

--- src/interface_adapter/CreateRequest/CreateRequestPresenter.java
+++ src/interface_adapter/CreateRequest/CreateRequestPresenter.java
@@ -1,4 +1,37 @@
 package interface_adapter.CreateRequest;
 
-public class CreateRequestPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.CreateRequest.CreateRequestOutputBoundary;
+import use_case.CreateRequest.CreateRequestOutputData;
+
+public class CreateRequestPresenter implements CreateRequestOutputBoundary {
+    private final CreateRequestViewModel createRequestViewModel;
+    private ViewRequestViewModel viewRequestViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public CreateRequestPresenter(ViewManagerModel viewManagerModel,
+                                  CreateRequestViewModel createRequestViewModel,
+                                  ViewRequestViewModel viewRequestViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.createRequestViewModel = createRequestViewModel;
+        this.viewRequestViewModel = viewRequestViewModel;
+    }
+    @Override
+    public void prepareSuccessView(CreateRequestOutputData response) {
+        ViewRequestState viewRequestState = this.viewRequestViewModel.getState();
+
+        viewRequestState.setRequest(response.getRequest());
+
+        this.viewRequestViewModel.setState(viewRequestState);
+        viewRequestViewModel.firePropertyChanged();
+        this.viewManagerModel.setActiveView(viewRequestViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        CreateRequestState createRequestState = createRequestViewModel.getState();
+        createRequestState.setCreateRequestError(error);
+        createRequestViewModel.firePropertyChanged();
+    }
 }
