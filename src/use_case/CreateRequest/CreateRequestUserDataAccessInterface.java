8b96cfa95576943e923599973253ffec33de7824
armaan_n <armaannanji@gmail.com>
2023-11-03 22:48:00 -0400

    added necessary methods to user data access interface

--- src/use_case/CreateRequest/CreateRequestUserDataAccessInterface.java
+++ src/use_case/CreateRequest/CreateRequestUserDataAccessInterface.java
@@ -1,4 +1,7 @@
 package use_case.CreateRequest;
 
+import entities.ServiceRequest;
+
 public interface CreateRequestUserDataAccessInterface {
+    public boolean saveRequest(ServiceRequest request);
 }
