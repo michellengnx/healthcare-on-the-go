410b7c8c06d9e48ea017fe4fe04422171c6bfc01
armaan_n <armaannanji@gmail.com>
2023-11-04 10:16:08 -0400

    implemented create request controller

--- src/interface_adapter/CreateRequest/CreateRequestController.java
+++ src/interface_adapter/CreateRequest/CreateRequestController.java
@@ -1,9 +1,27 @@
 package interface_adapter.CreateRequest;
 
 import entities.Patient;
+import entities.Service;
 import use_case.CreateRequest.CreateRequestInputBoundary;
 import use_case.CreateRequest.CreateRequestInputData;
 
-public class CreateRequestController {
+import java.util.Date;
 
+public class CreateRequestController {
+    final CreateRequestInputBoundary createRequestInteractor;
+    public CreateRequestController(CreateRequestInputBoundary createRequestInteractor) {
+        this.createRequestInteractor = createRequestInteractor;
+    }
+
+    public void execute(Date creationTime, int urgencyLevel, String destination, Service service, Patient user) {
+        CreateRequestInputData createRequestInputData = new CreateRequestInputData(
+                creationTime,
+                urgencyLevel,
+                destination,
+                service,
+                user
+        );
+
+        createRequestInteractor.execute(createRequestInputData);
+    }
 }
