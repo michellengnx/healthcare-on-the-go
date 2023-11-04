1ce0750bcd2eb565019ed4834547c159957176f4
armaan_n <armaannanji@gmail.com>
2023-11-03 22:44:31 -0400

    added necessary methods to doctor data access interface

--- src/use_case/CreateRequest/CreateRequestDoctorDataAccessInterface.java
+++ src/use_case/CreateRequest/CreateRequestDoctorDataAccessInterface.java
@@ -1,4 +1,9 @@
 package use_case.CreateRequest;
 
+import entities.Doctor;
+
+import java.util.List;
+
 public interface CreateRequestDoctorDataAccessInterface {
+    public List<Doctor> getDoctors();
 }
