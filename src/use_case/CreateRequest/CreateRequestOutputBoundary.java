cebaf1eb8ac7c272a69762beab612c0b32d31dd3
armaan_n <armaannanji@gmail.com>
2023-11-03 22:46:47 -0400

    added necessary methods to output boundary

--- src/use_case/CreateRequest/CreateRequestOutputBoundary.java
+++ src/use_case/CreateRequest/CreateRequestOutputBoundary.java
@@ -1,4 +1,8 @@
 package use_case.CreateRequest;
 
+import entities.ServiceRequest;
+
 public interface CreateRequestOutputBoundary {
+    public void prepareSuccessView(ServiceRequest request);
+    public void prepareFailView(String error);
 }
