c50deaf0d613c3f2b499f86f1cadc9a28e6656d3
armaan_n <armaannanji@gmail.com>
2023-11-04 10:27:29 -0400

    refactored interactor to use output data object

--- src/use_case/CreateRequest/CreateRequestOutputData.java
+++ src/use_case/CreateRequest/CreateRequestOutputData.java
@@ -1,4 +1,22 @@
 package use_case.CreateRequest;
 
+import entities.Patient;
+import entities.ServiceRequest;
+
 public class CreateRequestOutputData {
+    private final ServiceRequest request;
+    private final Patient patient;
+
+    public CreateRequestOutputData(ServiceRequest request, Patient patient) {
+        this.request = request;
+        this.patient = patient;
+    }
+
+    public ServiceRequest getRequest() {
+        return request;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
 }
