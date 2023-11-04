e439938a450e91dee0a95b91ce1bcff22d6830df
armaan_n <armaannanji@gmail.com>
2023-11-03 22:43:01 -0400

    added necessary methods to api access interface

--- src/use_case/CreateRequest/CreateRequestApiAccessInterface.java
+++ src/use_case/CreateRequest/CreateRequestApiAccessInterface.java
@@ -1,4 +1,7 @@
 package use_case.CreateRequest;
 
 public interface CreateRequestApiAccessInterface {
+    public float getDistance(String startLoc, String endLoc);
+    public float getEta(String startLoc, String endLoc);
+    public float getPrice(String startLoc, String endLoc);
 }
