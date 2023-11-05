64a9aa4fc304ed0f72082288375f4019fe73b9ba
armaan_n <armaannanji@gmail.com>
2023-11-04 20:00:00 -0400

    created create request view items, no logic has been implemented

---
+++ src/view/CreateRequestView.java
@@ -0,0 +1,71 @@
+package view;
+
+import interface_adapter.CreateRequest.CreateRequestController;
+import interface_adapter.CreateRequest.CreateRequestState;
+import interface_adapter.CreateRequest.CreateRequestViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Date;
+
+public class CreateRequestView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "create request";
+
+    private final CreateRequestViewModel createRequestViewModel;
+
+    private final Integer[] urgencyLevels = {1, 2, 3};
+    private final String[] availableServiceNames = {
+            "Vaccination",
+            "Nutritional support",
+            "Pharmaceutical services",
+            "General health check",
+            "X-ray imaging",
+            "Blood testing",
+            "Psychological care"
+    };
+    private final JComboBox<Integer> urgencyLevelComboBox = new JComboBox<>(urgencyLevels);
+    private final JComboBox<String> availableServiceComboBox = new JComboBox<>(availableServiceNames);
+    private final JTextField destinationInputField = new JTextField(30);
+    private final CreateRequestController createRequestController;
+
+    private final JButton createRequest;
+    private final JButton cancel;
+
+    public CreateRequestView(CreateRequestController controller, CreateRequestViewModel createRequestViewModel) {
+        this.createRequestController = controller;
+        this.createRequestViewModel = createRequestViewModel;
+        createRequestViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel(createRequestViewModel.TITLE_LABEL);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        LabelComboBoxPanel urgencyLevelSelect = new LabelComboBoxPanel(
+                new JLabel(createRequestViewModel.URGENCY_LABEL), urgencyLevelComboBox
+        );
+        LabelComboBoxPanel serviceSelect = new LabelComboBoxPanel(
+                new JLabel(createRequestViewModel.SERVICE_LABEL), availableServiceComboBox
+        );
+        LabelTextPanel destinationInfo = new LabelTextPanel(
+                new JLabel(createRequestViewModel.DESTINATION_LABEL), destinationInputField);
+
+        JPanel buttons = new JPanel();
+        createRequest = new JButton(createRequestViewModel.CREATE_REQUEST_BUTTON_LABEL);
+        buttons.add(createRequest);
+        cancel = new JButton(createRequestViewModel.CANCEL_BUTTON_LABEL);
+        buttons.add(cancel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+}
