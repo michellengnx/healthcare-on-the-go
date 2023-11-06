2d9869cba777f0dd14c18d3e16564cc7c019972e
armaan_n <armaannanji@gmail.com>
2023-11-06 17:50:00 -0500

    added documentation and comments to all use case classes and interfaces

--- src/use_case/ReturnHome/ReturnHomeOutputBoundary.java
+++ src/use_case/ReturnHome/ReturnHomeOutputBoundary.java
@@ -1,5 +1,14 @@
 package use_case.ReturnHome;
 
+/**
+ * Output boundary for the create request presenter. The presenter must implement this adhere to Clean Architecture, and
+ * higher and equal policy level classes (namely the interactor) must refer to the output boundary rather than the
+ * presenter itself. Lower policy level classes ought to use the input boundary as the variable type when instantiating
+ * the interactor.
+ */
 public interface ReturnHomeOutputBoundary {
+    /**
+     * Change the current view to the home screen.
+     */
     public void prepareSuccessView();
 }
