0daad71930e1f5d024b84d982d9ac9374c92ce53
armaan_n <armaannanji@gmail.com>
2023-11-06 17:50:00 -0500

    added documentation and comments to all use case classes and interfaces

--- src/use_case/ReturnHome/ReturnHomeInteractor.java
+++ src/use_case/ReturnHome/ReturnHomeInteractor.java
@@ -1,12 +1,23 @@
 package use_case.ReturnHome;
 
-public class ReturnHomeInteractor implements ReturnHomeInputBoundary{
+/**
+ * Use case interactor to return to the home screen.
+ */
+public class ReturnHomeInteractor implements ReturnHomeInputBoundary {
     ReturnHomeOutputBoundary returnHomePresenter;
 
+    /**
+     * Create ReturnHomeInteractor object.
+     *
+     * @param returnHomePresenter The presenter called upon when the use case is executed.
+     */
     public ReturnHomeInteractor(ReturnHomeOutputBoundary returnHomePresenter) {
         this.returnHomePresenter = returnHomePresenter;
     }
 
+    /**
+     * Call on the presenter, setting the view to the home screen.
+     */
     @Override
     public void execute() {
         this.returnHomePresenter.prepareSuccessView();
