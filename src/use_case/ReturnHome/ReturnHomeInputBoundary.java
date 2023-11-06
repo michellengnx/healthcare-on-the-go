eb029191d5432d24d93046f694c1fa06ec81a2fc
armaan_n <armaannanji@gmail.com>
2023-11-06 17:50:00 -0500

    added documentation and comments to all use case classes and interfaces

--- src/use_case/ReturnHome/ReturnHomeInputBoundary.java
+++ src/use_case/ReturnHome/ReturnHomeInputBoundary.java
@@ -1,5 +1,13 @@
 package use_case.ReturnHome;
 
+/**
+ * Input boundary for the return home interactor. The interactor must implement this adhere to Clean Architecture
+ * Lower policy level classes ought to use the input boundary as the variable type when instantiating the interactor.
+ */
 public interface ReturnHomeInputBoundary {
+
+    /**
+     * Execute the return home use case, i.e. set the current view to the home screen.
+     */
     public void execute();
 }
