58ac29d5d55d330708e13d6e631f567687b7cfa1
armaan_n <armaannanji@gmail.com>
2023-11-06 16:37:32 -0500

    added documentation and comments to all use case classes and interfaces

--- src/use_case/CreateRequest/CreateRequestInputBoundary.java
+++ src/use_case/CreateRequest/CreateRequestInputBoundary.java
@@ -1,5 +1,16 @@
 package use_case.CreateRequest;
 
+/**
+ * Input boundary for the create request interactor. The interactor must implement this adhere to Clean Architecture
+ * Lower policy level classes ought to use the input boundary as the variable type when instantiating the interactor.
+ */
 public interface CreateRequestInputBoundary {
+    /**
+     * Perform the use case given some input data, in particular, match the patient with a doctor, create the necessary
+     * request, mark the doctor as busy, add the request to the patient's list, and call the presenter. In the case that
+     * a doctor cannot be matched, remain on the same screen and call upon the presenter to display an error message.
+     *
+     * @param createRequestInputData Input data necessary to create a request for medical services.
+     */
     public void execute(CreateRequestInputData createRequestInputData);
 }
--- src/use_case/CreateRequest/CreateRequestInputData.java
+++ src/use_case/CreateRequest/CreateRequestInputData.java
@@ -5,6 +5,9 @@ import entities.Service;
 
 import java.util.Date;
 
+/**
+ * Input data object that stores the necessary values to create a request.
+ */
 public class CreateRequestInputData {
     private final Date creationTime;
     private final int urgencyLevel;
@@ -12,6 +15,15 @@ public class CreateRequestInputData {
     private final Service service;
     private final Patient patient;
 
+    /**
+     * Create a CreateRequestInputData object that contains the given values.
+     *
+     * @param creationTime The time at which the request was created.
+     * @param urgencyLevel An integer value denoting how urgent the request is (1 being the least urgent, and 3 being the most).
+     * @param destination Where the request is to be carried out.
+     * @param service The service requested.
+     * @param patient The patient who requested the service.
+     */
     public CreateRequestInputData(Date creationTime, int urgencyLevel, String destination, Service service, Patient patient) {
         this.creationTime = creationTime;
         this.urgencyLevel = urgencyLevel;
@@ -24,6 +36,9 @@ public class CreateRequestInputData {
         return creationTime;
     }
 
+    /**
+     * @return An integer between 1 and 3 denoting the urgency level (1 being the least urgent)
+     */
     public int getUrgencyLevel() {
         return urgencyLevel;
     }
