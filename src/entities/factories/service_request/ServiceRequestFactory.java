5a4e1f301c5a66009b14eb6b511acd1ea3307996
armaan_n <armaannanji@gmail.com>
2023-11-06 15:28:12 -0500

    added space

--- src/entities/factories/service_request/ServiceRequestFactory.java
+++ src/entities/factories/service_request/ServiceRequestFactory.java
@@ -8,6 +8,6 @@ import java.util.Date;
 
 public class ServiceRequestFactory {
     public ServiceRequest create(Date creationTime, Doctor doctor, int urgencyLevel, String destination, Service service, float price, float eta, float distance) {
-        return new ServiceRequest(creationTime, doctor,urgencyLevel, destination, service, price, eta, distance);
+        return new ServiceRequest(creationTime, doctor, urgencyLevel, destination, service, price, eta, distance);
     }
 }
