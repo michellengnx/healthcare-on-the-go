f5a92e83990c9e9734af9a092e4d814af5da315e
armaan_n <armaannanji@gmail.com>
2023-11-06 19:04:37 -0500

    added documentation and comments to the return home use case factory, and refactored the class to only use one method

--- src/app/ReturnHomeUseCaseFactory.java
+++ src/app/ReturnHomeUseCaseFactory.java
@@ -7,12 +7,19 @@ import use_case.ReturnHome.ReturnHomeInputBoundary;
 import use_case.ReturnHome.ReturnHomeInteractor;
 import use_case.ReturnHome.ReturnHomeOutputBoundary;
 
+/**
+ * Factory used to create controller for the return home use case.
+ */
 public class ReturnHomeUseCaseFactory {
+    /**
+     * Return a ReturnHomeController that executes the return home use case in its entirety, i.e., set the current view
+     * to that of the home screen.
+     *
+     * @param viewManagerModel The view model whose active view is to be set to the home screen.
+     * @param homeScreenViewModel The view model whose view corresponds to the home screen.
+     * @return A controller used to execute the return home use case.
+     */
     public static ReturnHomeController create(ViewManagerModel viewManagerModel, HomeScreenViewModel homeScreenViewModel) {
-        return createReturnHomeUseCase(viewManagerModel, homeScreenViewModel);
-    }
-
-    public static ReturnHomeController createReturnHomeUseCase(ViewManagerModel viewManagerModel, HomeScreenViewModel homeScreenViewModel) {
         ReturnHomeOutputBoundary returnHomeOutputBoundary = new ReturnHomePresenter(viewManagerModel, homeScreenViewModel);
 
         ReturnHomeInputBoundary returnHomeInteractor = new ReturnHomeInteractor(returnHomeOutputBoundary);
