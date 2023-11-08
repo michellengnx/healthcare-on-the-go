327c3800823eb44bc6010985e1e432255efbfb81
armaan_n <armaannanji@gmail.com>
2023-11-08 17:43:11 -0500

    added documentation to the interface adapters

--- src/interface_adapter/CreateRequest/CreateRequestViewModel.java
+++ src/interface_adapter/CreateRequest/CreateRequestViewModel.java
@@ -6,6 +6,10 @@ import interface_adapter.ViewModel;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * A view model used to store the CreateRequestView's labels and state. Also responsible for alerting the view of
+ * changes in the state.
+ */
 public class CreateRequestViewModel extends ViewModel {
     public final String TITLE_LABEL = "Create Request";
     public final String URGENCY_LABEL = "Choose Urgency Level";
@@ -16,6 +20,11 @@ public class CreateRequestViewModel extends ViewModel {
     private CreateRequestState state;
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
+    /**
+     * Create a CreateRequestViewModel object with a given patient.
+     *
+     * @param patient The patient creating the request.
+     */
     public CreateRequestViewModel(Patient patient) {
         super("create request");
         this.state = new CreateRequestState(patient);
@@ -29,11 +38,19 @@ public class CreateRequestViewModel extends ViewModel {
         return state;
     }
 
+    /**
+     * Alert the Views in this.support of changes made to the state.
+     */
     @Override
     public void firePropertyChanged() {
         this.support.firePropertyChange("state", null, this.state);
     }
 
+    /**
+     * Add a View that would like to listen for changes in the state.
+     *
+     * @param listener The View that would like to listen to the state stored in this object.
+     */
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
