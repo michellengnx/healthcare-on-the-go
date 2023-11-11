ac7c92488288e974b642fc76cab760e9025e6d86
armaan_n <armaannanji@gmail.com>
2023-11-10 21:56:40 -0500

    defined output boundary for resolve request use case

--- src/use_case/ResolveRequest/ResolveRequestOutputBoundary.java
+++ src/use_case/ResolveRequest/ResolveRequestOutputBoundary.java
@@ -1,4 +1,16 @@
 package use_case.ResolveRequest;
 
+/**
+ * Output boundary for the resolve request presenter. THe presenter must implement this to adhere to Clean Architecture,
+ * and higher or equal policy level classes (namely the interactor) must refer to this interface, rather than the
+ * presenter itself. Lower policy level classes ought to use the output boundary as the variable type when instantiating
+ * the presenter.
+ */
 public interface ResolveRequestOutputBoundary {
+    /**
+     * Update the ViewRequest view to show that the request has been resolved.
+     *
+     * @param response Data object containing the resolved request and the patient to which it belongs.
+     */
+    public void prepareSuccessView(ResolveRequestOutputData response);
 }
