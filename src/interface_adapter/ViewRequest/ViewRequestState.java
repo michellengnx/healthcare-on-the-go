360cdd85d5322b07bdc04faa67c35a0a292b227d
armaan_n <armaannanji@gmail.com>
2023-11-11 13:42:03 -0500

    implemented presenter for the resolve request use case. Also added empty dependent classes that need to be implemented.

---
+++ src/interface_adapter/ViewRequest/ViewRequestState.java
@@ -0,0 +1,14 @@
+package interface_adapter.ViewRequest;
+
+import entities.ServiceRequest;
+
+// TODO : IMPLEMENT
+
+public class ViewRequestState {
+
+    public void setRequest(ServiceRequest request) {
+    }
+
+    public void setViewRequestError(String error) {
+    }
+}
