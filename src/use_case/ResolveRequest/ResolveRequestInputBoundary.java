4144608afe5a9dbc3715eed37fb6dad9e29af266
armaan_n <armaannanji@gmail.com>
2023-11-10 21:46:55 -0500

    refactored files to accomodate new source directory

--- src/use_case/ResolveRequest/ResolveRequestInputBoundary.java
+++ src/use_case/ResolveRequest/ResolveRequestInputBoundary.java
@@ -1,4 +1,15 @@
-package org.example.src.use_case.ResolveRequest;
+package use_case.ResolveRequest;
 
+/**
+ *
+ */
 public interface ResolveRequestInputBoundary {
+    /**
+     * Execute the resolve request use case. Mark the patient's request as resolved, and subsequently inform the
+     * presenter.
+     *
+     * @param resolveRequestInputData Input data necessary to resolve a request, namely the patient and the request
+     *                                to be resolved.
+     */
+    public void execute(ResolveRequestInputData resolveRequestInputData);
 }
