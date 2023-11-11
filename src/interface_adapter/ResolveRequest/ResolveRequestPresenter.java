9e7d7f215d31710c9c8417a6b5c45263b774f95e
armaan_n <armaannanji@gmail.com>
2023-11-11 14:02:09 -0500

    add documentation to the resolve request presenter and implemented the view manager model

--- src/interface_adapter/ResolveRequest/ResolveRequestPresenter.java
+++ src/interface_adapter/ResolveRequest/ResolveRequestPresenter.java
@@ -5,13 +5,26 @@ import interface_adapter.ViewRequest.ViewRequestViewModel;
 import use_case.ResolveRequest.ResolveRequestOutputBoundary;
 import use_case.ResolveRequest.ResolveRequestOutputData;
 
+/**
+ * Presenter that will update the request being shown.
+ */
 public class ResolveRequestPresenter implements ResolveRequestOutputBoundary {
     private final ViewRequestViewModel viewRequestViewModel;
 
+    /**
+     * Create ViewRequestPresenter object given the view model of the view request use case.
+     *
+     * @param viewRequestViewModel The view model belonging to the view request use case.
+     */
     public ResolveRequestPresenter(ViewRequestViewModel viewRequestViewModel) {
         this.viewRequestViewModel = viewRequestViewModel;
     }
 
+    /**
+     * Update the request being shown.
+     *
+     * @param response Data object containing the updated request and the patient to which it belongs.
+     */
     @Override
     public void prepareSuccessView(ResolveRequestOutputData response) {
         // Create a new ViewRequestState whose response instance attribute is the resolved response
@@ -23,6 +36,11 @@ public class ResolveRequestPresenter implements ResolveRequestOutputBoundary {
         this.viewRequestViewModel.firePropertyChanged();
     }
 
+    /**
+     * Set the error message of the viewRequest state.
+     *
+     * @param error The error message to be sent.
+     */
     @Override
     public void prepareFailView(String error) {
         // Create a new CreateRequestState with the given error message
