f773a2db2eba55f53405d383135e9f0bdbaff6d8
armaan_n <armaannanji@gmail.com>
2023-11-10 21:52:32 -0500

    implemented resolve request output data object

--- src/use_case/ResolveRequest/ResolveRequestOutputData.java
+++ src/use_case/ResolveRequest/ResolveRequestOutputData.java
@@ -1,4 +1,31 @@
 package use_case.ResolveRequest;
 
+import entities.ServiceRequest;
+import org.example.src.entities.Patient;
+
+/**
+ * Output data object that stores the a resolved request and corresponding patient.
+ */
 public class ResolveRequestOutputData {
+    private final Patient patient;
+    private final ServiceRequest request;
+
+    /**
+     * Create a ResolveRequestOutputData object that contains the given values.
+     *
+     * @param patient The patient whose request was resolved.
+     * @param request The request that was resolved.
+     */
+    public ResolveRequestOutputData(Patient patient, ServiceRequest request) {
+        this.patient = patient;
+        this.request = request;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public ServiceRequest getRequest() {
+        return request;
+    }
 }
