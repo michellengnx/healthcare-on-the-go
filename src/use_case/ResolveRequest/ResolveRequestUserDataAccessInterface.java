e6bc27aa70498499757f9dd58127c11febb9466c
armaan_n <armaannanji@gmail.com>
2023-11-11 08:50:17 -0500

    defined user data access interface for the resolve request interactor

--- src/use_case/ResolveRequest/ResolveRequestUserDataAccessInterface.java
+++ src/use_case/ResolveRequest/ResolveRequestUserDataAccessInterface.java
@@ -1,4 +1,17 @@
 package use_case.ResolveRequest;
 
+import entities.ServiceRequest;
+import org.example.src.entities.Patient;
+
+/**
+ * Methods to user data access object must implement for the resolve request interactor to function
+ */
 public interface ResolveRequestUserDataAccessInterface {
+    /**
+     * Mark a given request as completed.
+     *
+     * @param patient The patient whose request has been completed.
+     * @param request The request to be marked as completed.
+     */
+    public void resolveRequest(Patient patient, ServiceRequest request);
 }
