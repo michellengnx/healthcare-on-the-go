fba87ebc221fb22426b611cf6115a8e98616a15c
izorakayumova <izora.kayumova@mail.utoronto.ca>
2023-11-11 23:18:56 -0500

    created an output data, output boundary and data access interface for edit profile use case

--- src/use_case/edit_profile/EditUserDataAccessInterface.java
+++ src/use_case/edit_profile/EditUserDataAccessInterface.java
@@ -1,2 +1,13 @@
-package org.example.src.use_case.edit_profile;public interface EditUserDataAccessInterface {
+package org.example.src.use_case.edit_profile;
+
+import org.example.src.entities.User;
+
+public interface EditUserDataAccessInterface {
+    boolean existsByName(String identifier);
+
+    void save(User user);
+
+    User get(String username);
+
+    String editProfile();
 }
