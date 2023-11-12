8a030abcfcc208d12a2d4639e5222c8fdbd84391
izorakayumova <izora.kayumova@mail.utoronto.ca>
2023-11-11 19:44:30 -0500

    created a controller responsible for executing the edit profile use case interaction

---
+++ src/interface_adapter/edit_profile/EditController.java
@@ -0,0 +1,32 @@
+package org.example.src.interface_adapter.edit_profile;
+
+import use_case.edit_profile.EditInputData;
+import use_case.edit_profile.EditInputBoundary;
+// do I need to import from signup?
+
+import java.util.Date;
+
+public class EditController {
+    final EditInputBoundary editUseCaseInteractor;
+
+    public EditController(EditInputBoundary editUseCaseInteractor) {
+        this.editUseCaseInteractor = editUseCaseInteractor;
+
+    }
+    // would this be newParameterName, rather than just parameterName?
+    /**
+     * Execute the edit profile use case, passing an input data object created using the arguments to the method.
+     * @param newPassword: new password the user wants to have associated with their profile.
+     * @param newEmail: new email the user wants to have associated with their profile.
+     * @param newPhoneNumber: new phone number the user wants to have associated with their profile.
+     * @param newInsurance: new insurance the user wants to have associated with their profile.
+     */
+
+    public void execute(String newUsername, String newPassword, String newEmail,
+                        String newPhoneNumber,String newInsurance) {
+        EditInputData editInputData = new EditInputData(
+                newUsername, newPassword, newEmail, newPhoneNumber, newInsurance);
+
+        editUseCaseInteractor.execute(editInputData);
+    }
+}
