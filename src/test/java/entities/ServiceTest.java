e0af5658aded1d799728f09ecf5037380ec69002
michellengnx <michellengnx@gmail.com>
2023-11-15 19:17:03 -0500

    fixed unit tests for Service

--- src/test/java/entities/ServiceTest.java
+++ src/test/java/entities/ServiceTest.java
@@ -1,13 +1,13 @@
 package entities;
 
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ServiceTest {
 
     @Test
     public void testServiceConstructor() {
         Service s = new Service("Annual checkup", 100);
-        assert s.getName().equals("Annual checkup");
-        assert s.getPrice() == 100;
+        assertEquals("Annual checkup", s.getName());
+        assertEquals(100, s.getPrice());
     }
 }
