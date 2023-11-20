4345a004cdd78a7fd942c18c9b41e2e7820bd0f3
izorakayumova <izora.kayumova@mail.utoronto.ca>
2023-11-19 19:03:58 -0500

    added exception to the patient data access object

--- src/main/java/data_access/FilePatientDataAccessObject.java
+++ src/main/java/data_access/FilePatientDataAccessObject.java
@@ -10,6 +10,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+// add implements SignupUserDataAccessInterface, LoginUserDataAccessInterface
 public class FilePatientDataAccessObject implements EditUserDataAccessInterface {
 
     private final File csvFile;
@@ -22,52 +23,56 @@ public class FilePatientDataAccessObject implements EditUserDataAccessInterface 
         csvFile = new File(csvPath);
         headers.put("username", 0);
         headers.put("password", 1);
-        headers.put("gender", 2);
-        headers.put("insurance", 3);
-        headers.put("birthday", 4);
-        headers.put("email", 5);
-        headers.put("number", 6);
-        headers.put("credit card", 7);
-        headers.put("emergency contact", 8);
-        headers.put("requests", 9);
+        headers.put("email", 2);
+        headers.put("phone number", 3);
+        headers.put("gender", 4);
+        headers.put("insurance", 5);
+        headers.put("birthday", 6);
+        headers.put("credit card number", 7);
+        headers.put("cvv", 8);
+        headers.put("credit card expiration date", 9);
+        headers.put("name on card", 10);
+        headers.put("emergency contact name", 11);
+        headers.put("emergency contact phone number", 12);
+        headers.put("emergency contact relationship", 13);
+        headers.put("requests", 14);
 
         if (csvFile.length() == 0) {
             save();
         } else {
+            try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+                String header = reader.readLine();
 
-            BufferedReader reader = new BufferedReader(new FileReader(csvFile));
-            String header = reader.readLine();
+                assert header.equals("username,password,email,phone number,gender,insurance,birthday,credit card number,cvv,credit card expiration date,name on card,emergency contact name,emergency contact phone number,emergency contact relationship,requests");
 
-            assert header.equals("username,password,email,number,gender,insurance,birthday,credit card number,cvv,credit card expiration date,name on card,emergency contact name,emergency contact phone number,emergency contact relationship,requests");
-
-            String row;
-            while ((row = reader.readLine()) != null) {
-                // does the type of credit card, birthday, emergency contact need to change?
-                String[] col = row.split(",");
-                String username = String.valueOf(col[headers.get("username")]);
-                String password = String.valueOf(col[headers.get("password")]);
-                String email = String.valueOf(col[headers.get("email")]);
-                String number = String.valueOf(col[headers.get("number")]);
-                String gender = String.valueOf(col[headers.get("gender")]);
-                String insurance = String.valueOf(col[headers.get("insurance")]);
-                String birthdate = String.valueOf(col[headers.get("birthday")]);
-                String creditCardNumber = String.valueOf(col[headers.get("credit card number")]);
-                int cvv = Integer.parseInt(col[headers.get("cvv")]);
-                String expirationDate = String.valueOf(col[headers.get("credit card expiration date")]);
-                String nameOnCard = String.valueOf(col[headers.get("name on card")]);
-                String emergencyContactName = String.valueOf(col[headers.get("emergency contact name")]);
-                String emergencyContactPhoneNumber = String.valueOf(col[headers.get("emergency contact phone number")]);
-                String emergencyContactRelationship = String.valueOf(col[headers.get("emergency contact relationship")]);
-                String requests = String.valueOf(col[headers.get("requests")]);
-                Date birthday = new SimpleDateFormat("dd/MM/yyyy").parse(birthdate);
-                CreditCard creditCard = new CreditCard(creditCardNumber, cvv, expirationDate, nameOnCard);
-                EmergencyContact emergencyContact = new EmergencyContact(emergencyContactName, emergencyContactPhoneNumber, emergencyContactRelationship);
-                Patient patient = new Patient(username, password, email, number, gender, insurance, birthday, creditCard, emergencyContact);
-                accounts.put(username, patient);
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    String username = String.valueOf(col[headers.get("username")]);
+                    String password = String.valueOf(col[headers.get("password")]);
+                    String email = String.valueOf(col[headers.get("email")]);
+                    String number = String.valueOf(col[headers.get("phone number")]);
+                    String gender = String.valueOf(col[headers.get("gender")]);
+                    String insurance = String.valueOf(col[headers.get("insurance")]);
+                    Date birthday = new SimpleDateFormat("dd/MM/yyyy").parse(String.valueOf(col[headers.get("birthday")]));
+                    String creditCardNumber = String.valueOf(col[headers.get("credit card number")]);
+                    int cvv = Integer.parseInt(col[headers.get("cvv")]);
+                    String expirationDate = String.valueOf(col[headers.get("credit card expiration date")]);
+                    String nameOnCard = String.valueOf(col[headers.get("name on card")]);
+                    String emergencyContactName = String.valueOf(col[headers.get("emergency contact name")]);
+                    String emergencyContactPhoneNumber = String.valueOf(col[headers.get("emergency contact phone number")]);
+                    String emergencyContactRelationship = String.valueOf(col[headers.get("emergency contact relationship")]);
+                    String requests = String.valueOf(col[headers.get("requests")]);
+                    CreditCard creditCard = new CreditCard(creditCardNumber, cvv, expirationDate, nameOnCard);
+                    EmergencyContact emergencyContact = new EmergencyContact(emergencyContactName, emergencyContactPhoneNumber, emergencyContactRelationship);
+                    Patient patient = new Patient(username, password, email, number, gender, insurance, birthday, creditCard, emergencyContact);
+                    accounts.put(username, patient);
+                }
             }
         }
     }
 
+    @Override
     public void save(Patient patient) {
         accounts.put(patient.getUsername(), patient);
         this.save();
@@ -114,17 +119,22 @@ public class FilePatientDataAccessObject implements EditUserDataAccessInterface 
         return !old.equals(updated);
     }
 
+    /**
+     * Return true if and only if the optional parameter was given a value and is not null.
+     * @param parameter could be new username, new password, new email, new insurance, new phone number.
+     * @return whether the value stored in the parameter is present and not null.
+     */
     private boolean optionPresent(String parameter) {
-        Optional param = Optional.ofNullable(parameter);
+        Optional<String> param = Optional.ofNullable(parameter);
         return param.isPresent();
     }
 
     public Integer editProfile(String username, String newUsername, String newPassword,
-                              String newInsurance, String newEmail, String newPhoneNumber) {
-        Integer changes = 0;
+                               String newEmail, String newPhoneNumber, String newInsurance) {
+        int changes = 0;
         Patient patient = accounts.get(username);
         if (optionPresent(newUsername) && !existsByName(newUsername)) {
-            // if there is no change between the usernames, the usernameOccupied would have caught it.
+            // if there is no change between the usernames, the exists by name method would have caught it.
             // Therefore, no need for changeExists function.
             patient.setUsername(newUsername);
             save();
@@ -133,16 +143,16 @@ public class FilePatientDataAccessObject implements EditUserDataAccessInterface 
             patient.setPassword(newPassword);
             save();
             changes += 1;
-        } if (optionPresent(newInsurance) && changeExists(patient.getInsurance(), newInsurance)) {
-            patient.setInsurance(newInsurance);
-            save();
-            changes += 1;
         } if (optionPresent(newEmail) && changeExists(patient.getEmail(), newEmail)) {
             patient.setEmail(newEmail);
             save();
             changes += 1;
         } if (optionPresent(newPhoneNumber) && changeExists(patient.getPhoneNumber(), newPhoneNumber)) {
             patient.setPhoneNumber(newPhoneNumber);
+            save();
+            changes += 1;
+        } if (optionPresent(newInsurance) && changeExists(patient.getInsurance(), newInsurance)) {
+            patient.setInsurance(newInsurance);
             save();
             changes += 1;
         }
