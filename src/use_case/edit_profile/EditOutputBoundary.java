0bd6bceaf75e55a47b9d567d866eea1716d89c38
izorakayumova <144392121+izorakayumova@users.noreply.github.com>
2023-11-21 16:04:50 -0500

    Delete src/use_case/edit_profile directory
    Accidental addition of these files through incorrect merging
    
    Signed-off-by: izorakayumova <144392121+izorakayumova@users.noreply.github.com>

--- src/use_case/edit_profile/EditOutputBoundary.java
+++
@@ -1,7 +0,0 @@
-package org.example.src.use_case.edit_profile;
-
-public interface EditOutputBoundary {
-    void prepareSuccessView(EditOutputData user);
-
-    void prepareFailView(String error);
-}
