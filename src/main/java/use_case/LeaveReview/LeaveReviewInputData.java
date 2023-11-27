e2f20a1ee24751e8fdbff2bebd9df27db80d96ae
Justin Huang <ajustin.huang@mail.utoronto.ca>
2023-11-27 13:33:55 -0500

    inputboundary and inputdata

--- src/main/java/use_case/LeaveReview/LeaveReviewInputData.java
+++ src/main/java/use_case/LeaveReview/LeaveReviewInputData.java
@@ -1,21 +1,31 @@
 package use_case.LeaveReview;
 import entities.Doctor;
-
 import java.util.Date;
 
 public class LeaveReviewInputData {
     String review;
     int stars;
     Doctor doctor;
+    Date reviewdate;
 
-    public LeaveReviewInputData(String review, int stars, Doctor doctor){
+    public LeaveReviewInputData(String review, int stars, Doctor doctor, Date reviewdate){
         this.review = review;
         this.stars = stars;
         this.doctor = doctor;
+        this.reviewdate = reviewdate;
 
     }
+    String getReview() {
+        return review;
+    }
+    int getStars() {
+        return stars;
+    }
+    Doctor getDoctor() {
+        return doctor;
+    }
 
-
-
-
+    Date getReviewDate() {
+        return reviewdate;
+    }
 }
--- src/main/java/view/LeaveReviewView.java
+++ src/main/java/view/LeaveReviewView.java
@@ -4,5 +4,5 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeListener;
 
-public class LeaveReviewView extends JPanel implements ActionListener, PropertyChangeListener {
+public class LeaveReviewView extends JPanel implements ActionListener, PropertyChangeListener {gigi
 }
