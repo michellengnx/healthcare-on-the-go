b43ce869fa224ba9c911f4547ea96766ba80f5fb
armaan-n <armaannanji@gmail.com>
2023-11-27 16:17:39 -0500

    implemented HomeScreenInputData

--- src/main/java/use_case/HomeScreen/HomeScreenInputData.java
+++ src/main/java/use_case/HomeScreen/HomeScreenInputData.java
@@ -1,4 +1,15 @@
 package use_case.HomeScreen;
 
 public class HomeScreenInputData {
+    private String viewName;
+
+    /**
+     * Create a HomeScreenInputData given the name of the view to switch to.
+     *
+     * @param viewName Name of the view to switch to.
+     */
+
+    public HomeScreenInputData(String viewName) {
+        this.viewName = viewName;
+    }
 }
