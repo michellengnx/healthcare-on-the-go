dd09efde1040fc827044f877731cfcd9711f656d
michellengnx <michellengnx@gmail.com>
2023-11-27 16:08:19 -0500

    added sign up controller

---
+++ src/main/java/interface_adapter/SignUp/SignUpController.java
@@ -0,0 +1,43 @@
+package interface_adapter.SignUp;
+
+import use_case.SignUp.SignUpInputBoundary;
+import use_case.SignUp.SignUpInputData;
+
+import java.util.Date;
+
+public class SignUpController {
+    final SignUpInputBoundary userSignUpUseCaseInteractor;
+    public SignUpController(SignUpInputBoundary userSignUpUseCaseInteractor) {
+        this.userSignUpUseCaseInteractor = userSignUpUseCaseInteractor;
+    }
+
+    public void execute(String username,
+                        String password,
+                        String repeatPassword,
+                        String email,
+                        String phoneNumber,
+                        String gender,
+                        String insurance,
+                        Date birthday,
+                        String creditCardNumber,
+                        int ccv,
+                        String expirationDate,
+                        String nameOnCard,
+                        String contactName,
+                        String contactPhoneNumber,
+                        String contactRelationship) {
+        SignUpInputData signUpInputData = new SignUpInputData(
+                username,
+                password,
+                repeatPassword,
+                email,
+                phoneNumber,
+                gender,
+                insurance,
+                birthday,
+                creditCardNumber,ccv,expirationDate,nameOnCard,
+                contactName,contactPhoneNumber,contactRelationship);
+
+        userSignUpUseCaseInteractor.execute(signUpInputData);
+    }
+}
