bbee20034437a1c8796c8fa5f0f6be609ebf6aff
Justin Huang <ajustin.huang@mail.utoronto.ca>
2023-11-27 14:46:19 -0500

    worked on the interactor, commeneted what needs to be implemented

--- src/main/java/use_case/LeaveReview/LeaveReviewInteractor.java
+++ src/main/java/use_case/LeaveReview/LeaveReviewInteractor.java
@@ -1,11 +1,11 @@
 package use_case.LeaveReview;
-import entities.*;
+import java.time.LocalDateTime;
+//Import..
 
-public class LeaveReviewInteractor {
+
+public class LeaveReviewInteractor implements LeaveReviewInputBoundary{
     LeaveReviewDoctorDataAccessInterface doctorDAO;
     LeaveReviewOutputBoundary presenter;
-
-
 
     public LeaveReviewInteractor(LeaveReviewDoctorDataAccessInterface doctorDAO,
                                   LeaveReviewOutputBoundary presenter){
@@ -13,9 +13,17 @@ public class LeaveReviewInteractor {
         this.presenter = presenter;
 
     }
+    @Override
     public void execute (LeaveReviewInputData leaveReviewInputData) {
+      //  if (doctor not in patient.orderhistory)
+        //userPresenter.prepareFailView("You can only rate Doctors you have received service from")
+        // else:
+
     //    review.doctor.settotalreviews(review.doctor.gettotalreviews() + 1);
      //   review.doctor.
+
+    }
+
 
 
 
