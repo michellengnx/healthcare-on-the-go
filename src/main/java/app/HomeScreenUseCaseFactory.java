8912dc78f65fa9b41183083b0a9e49d3a4d31dee
armaan_n <armaannanji@gmail.com>
2023-11-28 13:56:13 -0500

    implemented home screen factory

---
+++ src/main/java/app/HomeScreenUseCaseFactory.java
@@ -0,0 +1,41 @@
+package app;
+
+import interface_adapter.CreateRequest.CreateRequestViewModel;
+import interface_adapter.HomeScreen.HomeScreenController;
+import interface_adapter.HomeScreen.HomeScreenPresenter;
+import interface_adapter.HomeScreen.HomeScreenViewModel;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.ViewRequest.ViewRequestViewModel;
+import use_case.HomeScreen.HomeScreenInputBoundary;
+import use_case.HomeScreen.HomeScreenInteractor;
+import use_case.HomeScreen.HomeScreenOutputBoundary;
+import view.HomeScreenView;
+
+public class HomeScreenUseCaseFactory {
+    public static HomeScreenView create(ViewManagerModel viewManagerModel,
+                                           CreateRequestViewModel createRequestViewModel,
+                                           ViewRequestViewModel viewRequestViewModel,
+                                           HomeScreenViewModel homeScreenViewModel) {
+        HomeScreenController homeScreenController = createHomeScreenUseCase(
+                viewManagerModel,
+                createRequestViewModel,
+                viewRequestViewModel);
+        return new HomeScreenView(homeScreenController, homeScreenViewModel);
+
+    }
+
+    private static HomeScreenController createHomeScreenUseCase(ViewManagerModel viewManagerModel,
+                                                                   CreateRequestViewModel createRequestViewModel,
+                                                                   ViewRequestViewModel viewRequestViewModel) {
+
+        HomeScreenOutputBoundary homeScreenOutputBoundary = new HomeScreenPresenter(
+                createRequestViewModel,
+                viewRequestViewModel,
+                viewManagerModel);
+
+        HomeScreenInputBoundary homeScreenInputBoundary = new HomeScreenInteractor(
+                homeScreenOutputBoundary);
+
+        return new HomeScreenController(homeScreenInputBoundary);
+    }
+}
