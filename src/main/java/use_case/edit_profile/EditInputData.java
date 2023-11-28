b19e884cf49d2131e2520ba710077a8dbc1516bb
izorakayumova <izora.kayumova@mail.utoronto.ca>
2023-11-28 03:36:16 -0500

    updated Input Data to allow more editable items

--- src/main/java/use_case/edit_profile/EditInputData.java
+++ src/main/java/use_case/edit_profile/EditInputData.java
@@ -6,13 +6,29 @@ public class EditInputData {
     final private String email;
     final private String phoneNumber;
     final private String insurance;
+    final private String emergencyName;
+    final private String emergencyNumber;
+    final private String emergencyRelationship;
+    final private String creditCardNumber;
+    final private Integer cvv;
+    final private String expirationDate;
+    final private String nameOnCard;
 
-    public EditInputData(String username, String password, String email, String phoneNumber, String insurance) {
+    public EditInputData(String username, String password, String email, String phoneNumber, String insurance,
+                         String emergencyName, String emergencyNumber, String emergencyRelationship,
+                         String creditCardNumber, Integer cvv, String expirationDate, String nameOnCard) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.insurance = insurance;
+        this.emergencyName = emergencyName;
+        this.emergencyNumber = emergencyNumber;
+        this.emergencyRelationship = emergencyRelationship;
+        this.creditCardNumber =  creditCardNumber;
+        this.cvv = cvv;
+        this.expirationDate = expirationDate;
+        this.nameOnCard = nameOnCard;
     }
 
     public String getUsername() {
@@ -33,5 +49,33 @@ public class EditInputData {
 
     public String getInsurance() {
         return insurance;
+    }
+
+    public String getEmergencyName() {
+        return emergencyName;
+    }
+
+    public String getEmergencyNumber() {
+        return emergencyNumber
+    }
+
+    public String getEmergencyRelationship() {
+        return emergencyRelationship;
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public Integer getCvv() {
+        return cvv;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public String getNameOnCard() {
+        return nameOnCard;
     }
 }
