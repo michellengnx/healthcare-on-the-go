4aa4028809b54148ee741963cdce2108cf58a52f
michellengnx <michellengnx@gmail.com>
2023-11-28 22:15:53 -0500

    added unit tests for Doctor class

---
+++ src/test/java/entities/DoctorTest.java
@@ -0,0 +1,50 @@
+package entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class DoctorTest {
+
+    private Doctor doctor;
+
+    @BeforeEach
+    public void setUp() {
+        // Set up a sample Doctor object for testing
+        List<String> certifications = Arrays.asList("Cert1", "Cert2");
+        List<Service> qualifiedServices = Arrays.asList(new Service("Service1", 100), new Service("Service2", 200));
+
+        doctor = new Doctor("username", "password", "email@example.com",
+                "123-456-7890", "Male", new Date(), 1, "Hospital", certifications, qualifiedServices);
+    }
+
+    @Test
+    public void testGetLocation() {
+        assertEquals("Hospital", doctor.getLocation(), "Location should match the initialized value");
+    }
+
+    @Test
+    public void testGetCertifications() {
+        List<String> expectedCertifications = Arrays.asList("Cert1", "Cert2");
+        assertEquals(expectedCertifications, doctor.getCertifications(), "Certifications should match the initialized value");
+    }
+
+    @Test
+    public void testGetQualifiedServices() {
+        List<Service> expectedServices = Arrays.asList(new Service("Service1",100), new Service("Service2", 100));
+        assertEquals(expectedServices, doctor.getQualifiedServices(), "Qualified services should match the initialized value");
+    }
+
+    @Test
+    public void testDoctorInitialization() {
+        assertNotNull(doctor, "Doctor object should be initialized");
+        assertEquals("username", doctor.getUsername(), "Username should match the initialized value");
+        // Add more assertions as needed for other properties
+    }
+}
