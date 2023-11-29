aceb990147d880f16a4ea97cb6b7c596630ce86a
izorakayumova <izora.kayumova@mail.utoronto.ca>
2023-11-29 03:05:28 -0500

    added subheading labels in edit view model

--- src/main/java/interface_adapter/edit_profile/EditViewModel.java
+++ src/main/java/interface_adapter/edit_profile/EditViewModel.java
@@ -8,15 +8,18 @@ import java.beans.PropertyChangeSupport;
 public class EditViewModel extends ViewModel {
 
     public static final String TITLE_LABEL = "Edit View";
+    public static final String SUBHEADING_MAIN_LABEL = "Main Profile Details";
     public static final String NEW_USERNAME_LABEL = "Enter new username";
     public static final String NEW_PASSWORD_LABEL = "Enter new password";
     public static final String NEW_EMAIL_LABEL = "Enter new email";
     public static final String NEW_PHONE_NUMBER_LABEL = "Enter new phone number";
     public static final String NEW_INSURANCE_LABEL = "Enter new insurance";
+    public static final String SUBHEADING_CREDIT_CARD_LABEL = "Credit Card Details";
     public static final String NEW_CREDIT_CARD_NUMBER_LABEL = "Enter new credit card number";
     public static final String NEW_CVV_LABEL = "Enter new CVV";
     public static final String NEW_EXPIRATION_DATE_LABEL = "Enter new expiration date";
     public static final String NEW_NAME_ON_CARD_LABEL = "Enter new name on card";
+    public static final String SUBHEADING_EMERGENCY_CONTACT_LABEL = "Emergency Contact Details";
     public static final String NEW_EMERGENCY_NAME_LABEL = "Enter new name of emergency contact";
     public static final String NEW_EMERGENCY_NUMBER_LABEL = "Enter new emergency contact's phone number";
     public static final String NEW_EMERGENCY_RELATIONSHIP_LABEL = "Enter new relationship with emergency contact";
