1ac4240fb7f68d919ea757c3be3d31f4e25cf57e
michellengnx <michellengnx@gmail.com>
2023-11-29 15:38:01 -0500

    fixed init to match up with custom field change; rewrite signUp.addActionListener, no longer update state every key stroke but rather get all values once sign up button is clicked

--- src/main/java/view/SignUpView.java
+++ src/main/java/view/SignUpView.java
@@ -1,17 +1,15 @@
 package view;
 
 import interface_adapter.SignUp.SignUpController;
-import interface_adapter.SignUp.SignUpState;
 import interface_adapter.SignUp.SignUpViewModel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Arrays;
 import java.util.Date;
 
 public class SignUpView extends JPanel implements ActionListener, PropertyChangeListener {
@@ -29,13 +27,14 @@ public class SignUpView extends JPanel implements ActionListener, PropertyChange
     private final JTextField genderInputField = new JTextField(15);
     private final JTextField insuranceInputField = new JTextField(15);
 
-    private final DateInputField birthdayInputField = new DateInputField(); // You should initialize this according to your requirements
+    private final DateInputField birthdayInputField = new DateInputField(15); // You should initialize this according to your requirements
     private final JTextField creditCardNumberInputField = new JTextField(15);
     private final JTextField contactNameInputField = new JTextField(15);
     private final JTextField contactPhoneNumberInputField = new JTextField(15);
     private final JTextField contactRelationshipInputField = new JTextField(15);
 
     private final JFormattedTextField ccvInputField = new JFormattedTextField(); // You should initialize this according to your requirements
+
     private final JTextField expirationDateInputField = new JTextField(15);
     private final JTextField nameOnCardInputField = new JTextField(15);
 
@@ -49,7 +48,7 @@ public class SignUpView extends JPanel implements ActionListener, PropertyChange
         this.signUpViewModel = signUpViewModel;
         signUpViewModel.addPropertyChangeListener(this);
         JLabel title = new JLabel(SignUpViewModel.TITLE_LABEL);
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        title.setAlignmentX(Component.LEFT_ALIGNMENT);
 
 
 // name + text field
@@ -80,7 +79,7 @@ public class SignUpView extends JPanel implements ActionListener, PropertyChange
                 new JLabel(SignUpViewModel.CONTACT_RELATIONSHIP_LABEL), contactRelationshipInputField);
 
         LabelNumberPanel ccvInfo = new LabelNumberPanel(
-                new JLabel(SignUpViewModel.CCV_LABEL), ccvInputField);
+                new JLabel(SignUpViewModel.CCV_LABEL), ccvInputField, 15);
         LabelTextPanel expirationDateInfo = new LabelTextPanel(
                 new JLabel(SignUpViewModel.EXPIRATION_DATE_LABEL), expirationDateInputField);
         LabelTextPanel nameOnCardInfo = new LabelTextPanel(
@@ -93,99 +92,34 @@ public class SignUpView extends JPanel implements ActionListener, PropertyChange
         cancel = new JButton(SignUpViewModel.CANCEL_BUTTON_LABEL);
         buttons.add(cancel);
 
-        signUp.addActionListener(
-                // This creates an anonymous subclass of ActionListener and instantiates it.
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent evt) {
-                        if (evt.getSource().equals(signUp)) {
-                            SignUpState currentState = signUpViewModel.getState();
+        signUp.addActionListener(evt -> {
+            // Get input values from the fields
+            String username = usernameInputField.getText();
+            String password = Arrays.toString(passwordInputField.getPassword());
+            String repeatPassword = Arrays.toString(repeatPasswordInputField.getPassword());
+            String email = emailInputField.getText();
+            String phoneNumber = phoneNumberInputField.getText();
+            String gender = genderInputField.getText();
+            String insurance = insuranceInputField.getText();
+            Date birthday = birthdayInputField.getSelectedDate(); // Assuming you have a method to get the selected date
+            String creditCardNumber = creditCardNumberInputField.getText();
+            String contactName = contactNameInputField.getText();
+            String contactPhoneNumber = contactPhoneNumberInputField.getText();
+            String contactRelationship = contactRelationshipInputField.getText();
+            int ccv = Integer.parseInt(ccvInputField.getText()); // Assuming ccvInputField contains a numeric value
+            String expirationDate = expirationDateInputField.getText();
+            String nameOnCard = nameOnCardInputField.getText();
 
-                            signUpController.execute(
-                                    currentState.getUsername(),
-                                    currentState.getPassword(),
-                                    currentState.getRepeatPassword(),
-                                    currentState.getEmail(),
-                                    currentState.getPhoneNumber(),
-                                    currentState.getGender(),
-                                    currentState.getInsurance(),
-                                    currentState.getBirthday(),
-                                    currentState.getCreditCardNumber(),
-                            currentState.getCcv(),
-                            currentState.getExpirationDate(),
-                            currentState.getNameOnCard(),
-                            currentState.getContactName(),
-                            currentState.getContactPhoneNumber(),
-                            currentState.getContactRelationship()
-
-                            );
-                        }
-                    }
-                }
-        );
-
-        cancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                System.exit(0);
-            }
+            // Call the execute method with the input values
+            signUpController.execute(
+                    username, password, repeatPassword, email, phoneNumber, gender, insurance,
+                    birthday, creditCardNumber, ccv, contactPhoneNumber, contactRelationship,
+                    contactName, expirationDate, nameOnCard
+            );
         });
-        usernameInputField.addKeyListener(
-                new KeyListener() {
-                    @Override
-                    public void keyTyped(KeyEvent e) {
-                        SignUpState currentState = signUpViewModel.getState();
-                        String text = usernameInputField.getText() + e.getKeyChar();
-                        currentState.setUsername(text);
-                        signUpViewModel.setState(currentState);
-                    }
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                    }
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-                    }
-                });
 
-        passwordInputField.addKeyListener(
-                new KeyListener() {
-                    @Override
-                    public void keyTyped(KeyEvent e) {
-                        SignUpState currentState = signUpViewModel.getState();
-                        currentState.setPassword(passwordInputField.getText() + e.getKeyChar());
-                        signUpViewModel.setState(currentState);
-                    }
 
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-
-                    }
-
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-
-                    }
-                }
-        );
-
-        repeatPasswordInputField.addKeyListener(
-                new KeyListener() {
-                    @Override
-                    public void keyTyped(KeyEvent e) {
-                        SignUpState currentState = signUpViewModel.getState();
-                        currentState.setRepeatPassword(repeatPasswordInputField.getText() + e.getKeyChar());
-                        signUpViewModel.setState(currentState); // Hmm, is this necessary?
-                    }
-
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-
-                    }
-
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-
-                    }
-                }
-        );
+        cancel.addActionListener(evt -> System.exit(0));
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
@@ -213,21 +147,18 @@ public class SignUpView extends JPanel implements ActionListener, PropertyChange
      */
     public void actionPerformed(ActionEvent evt) {
         System.out.println("Click " + evt.getActionCommand());
-        JOptionPane.showConfirmDialog(this, "Cancel not implemented yet.");
+        JOptionPane.showConfirmDialog(this, "Invalid action");
     }
 
+
+    /**
+     * This method gets called when a bound property is changed.
+     *
+     * @param evt A PropertyChangeEvent object describing the event source
+     *            and the property that has changed.
+     */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
 
-        String propertyName = evt.getPropertyName();
-
-        if (propertyName.equals("signup")) {
-            SignUpState state = (SignUpState) evt.getNewValue();
-            System.out.println(state);
-            if (state.getUsernameError() != null) {
-                JOptionPane.showMessageDialog(this, state.getUsernameError());
-            }
-        }
-
-        }
     }
+}
