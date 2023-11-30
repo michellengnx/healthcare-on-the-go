61516d61e4149fc3a4ed1ecde2afda07527d489d
Justin Huang <ajustin.huang@mail.utoronto.ca>
2023-11-30 18:00:53 -0500

    commit 2

--- src/main/java/interface_adapter/LeaveReview/LeaveReviewState.java
+++ src/main/java/interface_adapter/LeaveReview/LeaveReviewState.java
@@ -1,7 +1,33 @@
 package interface_adapter.LeaveReview;
 
+import entities.Doctor;
+
+
 public class LeaveReviewState {
     private Integer stars = 4;
+    private String message = null;
+    private final Doctor doctor = null;
 
+
+    public Integer getStars() {
+        return stars;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public Doctor getDoctor() {
+        return null;
+    }
+
+    public void setStars(Integer stars) {
+        this.stars = stars;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
 }
+
+
+
