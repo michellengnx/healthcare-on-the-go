84f5c7a240e44fb51339a9e23a972ff4753b49dd
armaan_n <armaannanji@gmail.com>
2023-11-30 00:27:28 -0500

    added okhttp and json dependencies, and implemented some basic methods in the ApiAccessObject

---
+++ src/main/java/data_access/ApiAccessObject.java
@@ -0,0 +1,130 @@
+package data_access;
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import use_case.CreateRequest.InvalidLocationException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApiAccessObject {
+    private String apiKey;
+
+    public ApiAccessObject(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    /**
+     * Get the distance from a start to an end location
+     *
+     * @param start The start location
+     * @param end The end location
+     * @return The distance from start to end
+     * @throws InvalidLocationException One of the inputted locations has invalid
+     */
+    public float getDistance(String start, String end) throws InvalidLocationException {
+        return requestDistanceAndEta(start, end).get("distance");
+    }
+
+    /**
+     * Get the ETA from a start to an end location
+     *
+     * @param start The start location
+     * @param end The end location
+     * @return The ETA from start to end
+     * @throws InvalidLocationException One of the inputted locations has invalid
+     */
+    public float getEta(String start, String end) throws InvalidLocationException {
+        return requestDistanceAndEta(start, end).get("eta");
+    }
+
+    /**
+     * Get the price from a start to an end location
+     *
+     * @param start The start location
+     * @param end The end location
+     * @return The travel price from start to end
+     * @throws InvalidLocationException One of the inputted locations has invalid
+     */
+    public float getPrice(String start, String end) throws InvalidLocationException {
+        return requestDistanceAndEta(start, end).get("distance") * 0.1f;
+    }
+
+    /**
+     * Return an image URL that displays a route from the start to the end location
+     *
+     * @param start The start location
+     * @param end The end location
+     * @return A URL for an image that displays a route from start to end
+     * @throws InvalidLocationException One of the inputted locations has invalid
+     */
+    public String getTrafficMap(String start, String end) throws InvalidLocationException {
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme("https")
+                .host("www.mapquestapi.com")
+                .addPathSegment("staticmap")
+                .addPathSegment("v5")
+                .addPathSegment("map")
+                .addQueryParameter("key", this.apiKey)
+                .addQueryParameter("start", start)
+                .addQueryParameter("end", end)
+                .build();
+
+        Request request = new Request.Builder().url(httpUrl).build();
+
+        try {
+            // execute and print API response
+            Response response = client.newCall(request).execute();
+            System.out.println("Response : " + response);
+            if (response.code() == 200) {
+                JSONObject responseBody = new JSONObject(response.body().string());
+                return responseBody.getString("url");
+            } else {
+                throw new InvalidLocationException("Invalid location");
+            }
+        } catch (IOException | JSONException e) {
+            throw new InvalidLocationException("Invalid location");
+        }
+    }
+
+    private Map<String, Float> requestDistanceAndEta(String start, String end) throws InvalidLocationException {
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme("https")
+                .host("www.mapquestapi.com")
+                .addPathSegment("directions")
+                .addPathSegment("v2")
+                .addPathSegment("route")
+                .addQueryParameter("key", this.apiKey)
+                .addQueryParameter("from", start)
+                .addQueryParameter("to", end)
+                .build();
+
+        Request request = new Request.Builder().url(httpUrl).build();
+
+        try {
+            // execute and print API response
+            Response response = client.newCall(request).execute();
+            System.out.println("Response : " + response);
+            if (response.code() == 200) {
+                JSONObject responseBody = new JSONObject(response.body().string());
+
+                JSONObject route = responseBody.getJSONObject("route");
+                Map<String, Float> distanceEtaMap = new HashMap<>();
+                distanceEtaMap.put("distance", route.getFloat("distance") / 10);
+                distanceEtaMap.put("eta", (float) route.getInt("time"));
+                return distanceEtaMap;
+            } else {
+                throw new InvalidLocationException("Invalid location");
+            }
+        } catch (IOException | JSONException e) {
+            throw new InvalidLocationException("Invalid location");
+        }
+    }
+
+}
