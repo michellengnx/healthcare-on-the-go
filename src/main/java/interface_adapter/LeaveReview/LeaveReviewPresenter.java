ee13846f81bffb359e27eef9448df85f139c70e1
justin <justinharvo1@gmail.com>
2023-12-02 13:39:36 -0500

    hello

--- src/main/java/interface_adapter/LeaveReview/LeaveReviewPresenter.java
+++ src/main/java/interface_adapter/LeaveReview/LeaveReviewPresenter.java
@@ -1,4 +1,27 @@
 package interface_adapter.LeaveReview;
 
-public class LeaveReviewPresenter {
+import use_case.LeaveReview.LeaveReviewOutputBoundary;
+import use_case.LeaveReview.LeaveReviewOutputData;
+
+public class LeaveReviewPresenter implements LeaveReviewOutputBoundary {
+    private final LeaveReviewViewModel leaveReviewViewModel;
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void prepareSuccessView(LeaveReviewOutputData review) {
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
