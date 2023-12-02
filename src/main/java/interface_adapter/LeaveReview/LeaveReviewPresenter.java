0810b9f347adb7d1ecc9749aed475c358430d2fe
justin <justinharvo1@gmail.com>
2023-12-02 16:35:53 -0500

    fixed presenter, working on test case

--- src/main/java/interface_adapter/LeaveReview/LeaveReviewPresenter.java
+++ src/main/java/interface_adapter/LeaveReview/LeaveReviewPresenter.java
@@ -1,17 +1,17 @@
 package interface_adapter.LeaveReview ;
 
-import interface_adapter.ViewManagerModel;
-import interface_adapter.ViewRequest.ViewRequestState;
-import interface_adapter.ViewRequest.ViewRequestViewModel;
-import use_case.CreateRequest.CreateRequestOutputData;
+
 import use_case.LeaveReview.LeaveReviewOutputBoundary;
 import use_case.LeaveReview.LeaveReviewOutputData;
 
 
 public class LeaveReviewPresenter implements LeaveReviewOutputBoundary {
+    private final LeaveReviewViewModel leaveReviewViewModel;
 
 
-    public LeaveReviewPresenter(LeaveReviewViewModel leaveReviewViewModel) {
+    public LeaveReviewPresenter(LeaveReviewViewModel leaveReviewViewModel1) {
+
+        this.leaveReviewViewModel = leaveReviewViewModel1;
     }
 
     @Override
