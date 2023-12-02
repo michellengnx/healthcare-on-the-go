f1e1329f3531722554dbe83e8fdbbd281fd1ea3a
izorakayumova <izora.kayumova@mail.utoronto.ca>
2023-12-02 18:47:16 -0500

    added subheading labels

--- src/main/java/interface_adapter/SignUp/SignUpViewModel.java
+++ src/main/java/interface_adapter/SignUp/SignUpViewModel.java
@@ -8,6 +8,7 @@ import java.beans.PropertyChangeSupport;
 public class SignUpViewModel extends ViewModel {
 
     public static final String TITLE_LABEL = "Sign Up View";
+    public static final String SUBHEADING_MAIN_LABEL = "Main Profile Details";
     public static final String USERNAME_LABEL = "Choose username";
     public static final String PASSWORD_LABEL = "Choose password";
     public static final String REPEAT_PASSWORD_LABEL = "Enter password again";
@@ -15,16 +16,16 @@ public class SignUpViewModel extends ViewModel {
     public static final String PHONE_NUMBER_LABEL = "Phone Number";
     public static final String GENDER_LABEL = "Gender";
     public static final String INSURANCE_LABEL = "Insurance";
-
     public static final String BIRTHDAY_LABEL = "Birthday";
+    public static final String SUBHEADING_CREDIT_CARD_LABEL = "Credit Card Details";
     public static final String CREDIT_CARD_NUMBER_LABEL = "Credit Card Number";
-    public static final String CONTACT_NAME_LABEL = "Contact Name";
-    public static final String CONTACT_PHONE_NUMBER_LABEL = "Contact Phone Number";
-    public static final String CONTACT_RELATIONSHIP_LABEL = "Contact Relationship";
-
     public static final String CCV_LABEL = "CCV";
     public static final String EXPIRATION_DATE_LABEL = "Expiration Date";
     public static final String NAME_ON_CARD_LABEL = "Name on Card";
+    public static final String SUBHEADING_EMERGENCY_CONTACT_LABEL = "Emergency Contact Details";
+    public static final String CONTACT_NAME_LABEL = "Contact Name";
+    public static final String CONTACT_PHONE_NUMBER_LABEL = "Contact Phone Number";
+    public static final String CONTACT_RELATIONSHIP_LABEL = "Contact Relationship";
 
 
 
