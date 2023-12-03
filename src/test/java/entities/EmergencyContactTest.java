86442f26113a5239f2de6494242ebce73d6c0a59
michellengnx <michellengnx@gmail.com>
2023-12-03 01:02:15 -0500

    fixed EmergencyContactTest

--- src/test/java/entities/EmergencyContactTest.java
+++ src/test/java/entities/EmergencyContactTest.java
@@ -5,15 +5,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EmergencyContactTest {
+
     @Test
-    public void testEmergencyContactConstructor() {
-        EmergencyContact emergencyContact = new EmergencyContact(
-                "Michelle",
-                "6473036832",
-                "friend"
-        );
-        assertEquals("Michelle",emergencyContact.getName());
-        assertEquals("6473036832",emergencyContact.getPhoneNumber());
-        assertEquals("friend",emergencyContact.getRelationship());
+    void testEmergencyContactConstructorAndGetters() {
+        // Arrange
+        String name = "John Doe";
+        String phoneNumber = "1234567890";
+        String relationship = "Friend";
+
+        // Act
+        EmergencyContact emergencyContact = new EmergencyContact(name, phoneNumber, relationship);
+
+        // Assert
+        assertEquals(name, emergencyContact.getName());
+        assertEquals(phoneNumber, emergencyContact.getPhoneNumber());
+        assertEquals(relationship, emergencyContact.getRelationship());
+    }
+
+    @Test
+    void testEmergencyContactSetters() {
+        // Arrange
+        EmergencyContact emergencyContact = new EmergencyContact("John Doe", "1234567890", "Friend");
+
+        // Act
+        String newName = "Jane Doe";
+        String newPhoneNumber = "9876543210";
+        String newRelationship = "Family";
+
+        emergencyContact.setName(newName);
+        emergencyContact.setPhoneNumber(newPhoneNumber);
+        emergencyContact.setRelationship(newRelationship);
+
+        // Assert
+        assertEquals(newName, emergencyContact.getName());
+        assertEquals(newPhoneNumber, emergencyContact.getPhoneNumber());
+        assertEquals(newRelationship, emergencyContact.getRelationship());
     }
 }
