1fbb2985d1e57e058d6f3eccdb6111aba1a4ad7f
michellengnx <michellengnx@gmail.com>
2023-12-03 00:59:30 -0500

    removed unused import

--- src/test/java/entities/CreditCardTest.java
+++ src/test/java/entities/CreditCardTest.java
@@ -1,8 +1,8 @@
 package entities;
 
-import entities.CreditCard;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreditCardTest {
 
