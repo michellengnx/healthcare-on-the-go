3ae6c7cf30a3dbf3a045fdcc740bd7e2e7dc0b76
Chonarue <ismael.chonarueda@mail.utoronto.ca>
2023-12-03 17:48:41 -0500

    Changed the color to blue no like

--- src/main/java/view/LockView.java
+++ src/main/java/view/LockView.java
@@ -18,6 +18,7 @@ import java.text.ParseException;
 
 public class LockView extends JPanel implements ActionListener, PropertyChangeListener {
     public String viewName = "lock";
+    private final Color myBlue = new Color(153, 204, 255);
     private JPanel buttonPanel;
     private final String patientDataPath = "data/patients.csv";
     private JButton login;
@@ -36,11 +37,14 @@ public class LockView extends JPanel implements ActionListener, PropertyChangeLi
 
 
     public LockView(LockViewModel lockViewModel, LockController lockController) throws IOException {
+        this.setBackground(myBlue);
         this.lockViewModel = lockViewModel;
         this.login = new JButton(lockViewModel.LOGIN_BUTTON_LABEL);
         this.signup = new JButton(lockViewModel.SIGNUP_BUTTON_LABEL);
         this.buttonPanel = new JPanel(new FlowLayout());
+        this.buttonPanel.setBackground(myBlue);
         this.labelPanel = new JPanel(new FlowLayout());
+        this.labelPanel.setBackground(myBlue);
         this.setSize(800,600);
         this.login.addActionListener(this);
         this.signup.addActionListener(this);
@@ -48,6 +52,7 @@ public class LockView extends JPanel implements ActionListener, PropertyChangeLi
         this.image = ImageIO.read(imageFile);
         this.scaledImage = image.getScaledInstance(600, 400, Image.SCALE_SMOOTH);
         this.imageLabel = new JLabel(new ImageIcon(scaledImage));
+        this.imageLabel.setBackground(myBlue);
 
 
         signup.addActionListener(
@@ -76,6 +81,7 @@ public class LockView extends JPanel implements ActionListener, PropertyChangeLi
 
         // Create a panel for labels using a BoxLayout (vertical arrangement)
         JPanel labelPanel = new JPanel();
+        labelPanel.setBackground(myBlue);
         labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
         labelPanel.setBorder(new EmptyBorder(100, 0, 100, 0)); // Adjust top and bottom margins as needed
 
@@ -117,6 +123,7 @@ public class LockView extends JPanel implements ActionListener, PropertyChangeLi
         buttonPanel.setBorder(new EmptyBorder(20, 0, 20, 0)); // Adjust top and bottom margins as needed
         buttonPanel.add(signup);
         buttonPanel.add(login);
+        buttonPanel.setBackground(myBlue);
 
         // Add the button panel to the SOUTH of the main panel
         this.add(buttonPanel, BorderLayout.SOUTH);
