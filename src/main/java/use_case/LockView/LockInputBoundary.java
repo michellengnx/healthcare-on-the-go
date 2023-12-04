509aa87cf2c199415d4ccbfc6381ceb786ab9538
izorakayumova <izora.kayumova@mail.utoronto.ca>
2023-12-03 19:31:15 -0500

    added javadoc to lock use case

--- src/main/java/use_case/LockView/LockInputBoundary.java
+++ src/main/java/use_case/LockView/LockInputBoundary.java
@@ -1,5 +1,9 @@
 package use_case.LockView;
 
+/**
+ * Represents the input boundary for locking the view.
+ * Implementations of this interface define the contract for locking operations.
+ */
 public interface LockInputBoundary {
     void execute(String viewName);
 }
