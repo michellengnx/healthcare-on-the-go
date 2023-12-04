e7cb796cc67c0f5d941dd49041c315a2ac731d44
izorakayumova <izora.kayumova@mail.utoronto.ca>
2023-12-03 19:34:05 -0500

    added javadoc to view request use case

--- src/main/java/use_case/ViewRequest/ViewRequestOutputBoundary.java
+++ src/main/java/use_case/ViewRequest/ViewRequestOutputBoundary.java
@@ -2,6 +2,10 @@ package use_case.ViewRequest;
 
 import use_case.SignUp.SignUpOutputData;
 
+
+/**
+ * Interface defining the output boundaries for viewing request details.
+ */
 public interface ViewRequestOutputBoundary {
     void prepareSuccessView(ViewRequestOutputData requests);
 
