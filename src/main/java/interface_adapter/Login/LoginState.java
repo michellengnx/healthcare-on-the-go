bbb9e7584029daf3d86b660389ed91f897eb16cc
michellengnx <michellengnx@gmail.com>
2023-12-03 22:32:06 -0500

    minor fixed in Login use case; changes in State error and how error(s) are handled

--- src/main/java/interface_adapter/Login/LoginState.java
+++ src/main/java/interface_adapter/Login/LoginState.java
@@ -2,16 +2,14 @@ package interface_adapter.Login;
 
 public class LoginState {
     private String username = "";
-    private String usernameError = null;
     private String password = "";
-    private String passwordError = null;
+    private String error = null;
 
-    public LoginState(LoginState copy) {
-        username = copy.username;
-        usernameError = copy.usernameError;
-        password = copy.password;
-        passwordError = copy.passwordError;
-    }
+//    public LoginState(LoginState copy) {
+//        username = copy.username;
+//        password = copy.password;
+//        error = copy.error;
+//    }
 
     // Because of the previous copy constructor, the default constructor must be explicit.
     public LoginState() {}
@@ -20,31 +18,19 @@ public class LoginState {
         return username;
     }
 
-    public String getUsernameError() {
-        return usernameError;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public String getPasswordError() {
-        return passwordError;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public void setUsernameError(String usernameError) {
-        this.usernameError = usernameError;
-    }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public void setPasswordError(String passwordError) {
-        this.passwordError = passwordError;
+    public void setError(String error) {
+        this.error = error;
     }
 }
