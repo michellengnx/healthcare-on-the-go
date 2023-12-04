e4027f4a628da8f48938f3491452ca9fbde3d7f0
michellengnx <michellengnx@gmail.com>
2023-12-03 22:50:35 -0500

    changed LockViewTest due to a change in LockView component

--- src/test/java/view/LockViewTest.java
+++ src/test/java/view/LockViewTest.java
@@ -1,70 +1,73 @@
-//package view;
-//
-//
-//import app.App;
-//import org.junit.jupiter.api.Test;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//
-//
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//
-//public class LockViewTest {
-//
-//
-//    /**
-//     * ensures there are at least 2 users in the CSV file for testing purposes
-//     */
-//
-//    public JButton getButton(int nth) {
-//        JFrame app = null;
-//        Window[] windows = Window.getWindows();
-//        for (Window window : windows) {
-//            System.out.print(window);
-//            if (window instanceof JFrame) {
-//                app = (JFrame) window;
-//            }
-//        }
-//
-//        assertNotNull(app); // found the window?
-//
-//        Component root = app.getComponent(0);
-//
-//        Component cp = ((JRootPane) root).getContentPane();
-//
-//        JPanel jp = (JPanel) cp;
-//
-//        JPanel jp2 = (JPanel) jp.getComponent(0);
-//
-//        LockView lockView = (LockView) jp2.getComponent(7);
-//
-//        JPanel buttonPanel = (JPanel) lockView.getComponent(0);
-//// 0 - signup, 1 - login
-//        return (JButton) buttonPanel.getComponent(nth);
-//    }
-//
-//    /**
-//     * Test that the Signup button is present and where it is expected to be
-//     */
-//    @Test
-//    public void testSignUpButtonPresent() {
-//        App.main(null);
-//        JButton button = getButton(0);
-//        assert (button.getText().equals("Sign up"));
-//    }
-//
-//    /**
-//     * Test that the Login button is present and where it is expected to be
-//     */
-//    @Test
-//    public void testLoginButtonPresent() {
-//        App.main(null);
-//        JButton button = getButton(1);
-//        assert (button.getText().equals("Log in"));
-//    }
-//
-//
-//
-//}
+package view;
+
+
+import app.App;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class LockViewTest {
+
+
+    /**
+     * ensures there are at least 2 users in the CSV file for testing purposes
+     */
+
+    public JButton getButton(int nth) {
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            System.out.print(window);
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+
+        assertNotNull(app); // found the window?
+
+        Component root = app.getComponent(0);
+
+        Component cp = ((JRootPane) root).getContentPane();
+
+        JPanel jp = (JPanel) cp;
+
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+
+        LockView lockView = (LockView) jp2.getComponent(7);
+
+        JPanel buttonPanel = (JPanel) lockView.getComponent(2);
+// 0 - signup, 1 - login
+        return (JButton) buttonPanel.getComponent(nth);
+    }
+
+    /**
+     * Test that the Signup button is present and where it is expected to be
+     */
+
+
+    @Test
+    public void testSignUpButtonPresent() throws IOException {
+        App.main(null);
+        JButton button = getButton(0);
+        assert (button.getText().equals("Sign up"));
+    }
+
+    /**
+     * Test that the Login button is present and where it is expected to be
+     */
+    @Test
+    public void testLoginButtonPresent() throws IOException {
+        App.main(null);
+        JButton button = getButton(1);
+        assert (button.getText().equals("Log in"));
+    }
+
+
+
+}
