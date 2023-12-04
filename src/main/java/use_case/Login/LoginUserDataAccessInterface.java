13491eeecf60d97308df51fcf5a09b9d89ea2945
michellengnx <michellengnx@gmail.com>
2023-12-03 22:36:52 -0500

    added save method to LoginUserDataAccessInterface

--- src/main/java/use_case/Login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/Login/LoginUserDataAccessInterface.java
@@ -1,10 +1,13 @@
 package use_case.Login;
 
 
+import entities.Patient;
 import entities.User;
 
 public interface LoginUserDataAccessInterface {
     boolean existsByUsername(String identifier);
 
     User get(String username);
+
+    void save(Patient samplePatient);
 }
